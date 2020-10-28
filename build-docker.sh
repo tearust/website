9f7f16914bbc0addc177dda14305e133331e6f96
kevingzhang <kevin.zhang.canada@gmail.com>
2020-10-27 21:52:15 -0700

    update

--- build-docker.sh
+++ build-docker.sh
@@ -1,12 +1,16 @@
-tag=0.3.5
+tag=0.3.6
+echo building backend...
 cd backend
+echo building docker backend...
 docker build . -t tearust/website-backend:$tag
 cd ..
-
+echo building frontend...
 cd frontend
 npm run build
+echo building docker frontend...
 docker build . -t tearust/website-frontend:$tag
 cd ..
+echo docker push
 
 docker push tearust/website-backend:$tag
 docker push tearust/website-frontend:$tag
