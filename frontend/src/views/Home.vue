cdbcbee6e8d1444ec7db94d2657592d267ef6058
kevingzhang <kevin.zhang.canada@gmail.com>
2020-10-28 17:11:18 -0700

    update

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,8 +2,10 @@
 <div class="c-page">
 
   <div style="display:flex; flex-direction:column; align-items:center; justify-content:center; padding: 24px 0 40px; margin: 0 auto; width: 800px;">
-    <h1 style="font-size: 56px; color: #444;font-weight: bold; text-align:center; font-family:sans-serif; line-height:50px;">Easily build decentralized apps</h1>
-    <p style="font-size: 32px; color: #111; text-align:center; line-height: 36px; margin-top: 15px;">TODO: please complete here.</p>
+    <h1 style="font-size: 56px; color: #444;font-weight: bold; text-align:center; font-family:sans-serif; line-height:50px;">Elevates Decentralized Trusted Computing to a T</h1>
+    <p style="font-size: 32px; color: #111; text-align:center; line-height: 36px; margin-top: 15px;">
+        TEA (Trusted Execution and Attestation) is an ambitials project aim to build new ways to build, deploy and run apps in pure decentralized manners.
+      </p>
   </div>
 
   <el-row class="c-block">
@@ -39,15 +41,10 @@
 
   <el-row class="c-text-image">
     <el-col :span="15">
-      <h2>T-rust elevates Decentralized Trusted Computing to a T</h2>
-      <p>
-        TEA (Trusted Execution and Attestation) is an ambitials project aim to build new ways to build, deploy and run apps in pure decentralized manners.
-      </p>
-      <p>
-        There won't be traditional server runs your code. Instead, all code and data are encrypted stored and executed in the p2p network. Only trusted hardware TEA modules can load and decrypt code or data then execute in hardware protected environment to guarantee security and correctness </p>
-     <!-- <p>None of Developer, service provider and end user need to trust any other parties. Blockchain smart contracts inspect the Proof-of-trust (PoT) data collected from hardware TPM chips. Only those pass remote attestation may get a chance to execute code and access to data. </p>
-     <p>Although the execution environment is protected by hardware, we still only allow customer executables running in highly isolated WebAssembly runtime to get highest security level</p>
-     <p>You do not know and do not care which node actually runs your code. you only need to trust silicon and math. </p> -->
+      <h2>Turn IPFS(File System) to IPFS(Function-as-a-Service)</h2>
+      <p>By adding a TEA trusted computing module, existing IPFS node can turn to a function-as-a-server node besides storing the code and data.</p>
+      <p>IPFS store and cache the code, TEA module load the code and execute</p>
+      <p>TEA protocol provides proof-of-trust (Tea Pot) for verification</p>
 
       <div>
         <el-button type="primary" size="small">I am Button</el-button>
@@ -56,23 +53,47 @@
       
     </el-col>
     <el-col :span="9" style="text-align:right;">
-      <el-image fit="cover" src="/img/brewing-tea.jpg" ></el-image>
+      <el-image fit="cover" src="https://github.com/tearust/tea-docs/blob/main/res/tea-on-top-of-ipfs.png?raw=true" ></el-image>
     </el-col>
   </el-row>
 
   <el-row class="c-text-image">
     <el-col :span="9" style="text-align:left;">
-      <el-image fit="cover" src="/img/brewing-tea.jpg" ></el-image>
+      <el-image fit="cover" src="https://github.com/tearust/tea-docs/blob/main/res/layer1-and-layer2.png?raw=true" ></el-image>
     </el-col>
     <el-col :span="15">
-      <h2>T-rust elevates Decentralized Trusted Computing to a T</h2>
-      <p>
-        TEA (Trusted Execution and Attestation) is an ambitials project aim to build new ways to build, deploy and run apps in pure decentralized manners.
-      </p>
-      <p>
-        There won't be traditional server runs your code. Instead, all code and data are encrypted stored and executed in the p2p network. Only trusted hardware TEA modules can load and decrypt code or data then execute in hardware protected environment to guarantee security and correctness </p>
-     
+      <h2>Take over complex computational workloads from blockchain as a trusted layer-2 oracle</h2>
+      <p>Smart contract running in blockchain (layer-1) call an external function that runs inside TEA network(layer-2). The function result feed back to smart contract with Proof-of-trust for verification</p>
+      <div style="text-align:left;">
+        <el-button type="primary" size="small">I am Button</el-button>
+        <a href="" style="margin-left: 24px;">I am link</a>
+      </div>
+    </el-col>
+    
+  </el-row>
+  <el-row class="c-text-image">
+    <el-col :span="15">
+      <h2>Hardware and crypto protected protocol ensures you own your own data</h2>
+      <p>Encrypt privacy data before upload to IPFS, distribute the decryption key to TEA modules that protected by hardware chips. Only trusted module can access your data after remote attestation</p>
 
+      <div>
+        <el-button type="primary" size="small">I am Button</el-button>
+        <a href="" style="margin-left: 24px;">I am link</a>
+      </div>
+      
+    </el-col>
+    <el-col :span="9" style="text-align:right;">
+      <el-image fit="cover" src="/img/logo.png" ></el-image>
+    </el-col>
+  </el-row>
+
+  <el-row class="c-text-image">
+    <el-col :span="9" style="text-align:left;">
+      <el-image fit="cover" src="https://github.com/tearust/tea-docs/blob/main/res/white_no_censorship_button.png?raw=true" ></el-image>
+    </el-col>
+    <el-col :span="15">
+      <h2>Build dApps that censorship-resistent</h2>
+      <p>Build dApps that encrypted data store in IPFS and code runs in TEA modules. Those kind of dApps cannot be stopped or controlled by any one, even the builder. </p>
       <div style="text-align:left;">
         <el-button type="primary" size="small">I am Button</el-button>
         <a href="" style="margin-left: 24px;">I am link</a>
@@ -84,7 +105,7 @@
   <div class="c-case">
     <h4>Use cases</h4>
     <h1>Build what's next</h1>
-    <b>With user-owned data and smart contracts, you can build new kinds of apps that couldn’t exist before.</b>
+    <b>With T-rust, you can build new kinds of apps that couldn’t exist before.</b>
     <el-row style="margin-top: 32px;">
       <el-col :span="11" class="x-col">
         <div class="x-1 flex-center">
@@ -129,35 +150,6 @@
     </el-row>
   </div>
 
-<!-- <div class="c-md">
- 
-  <el-row class="p-t1">
-    <hr/>
-    <h3>Turn IPFS(File System) to IPFS(Function-as-a-Service)</h3>
-    <div class="">By adding a TEA trusted computing module, existing IPFS node can turn to a function-as-a-server node besides storing the code and data.</div>
-  <div class="">
-    By adding a TEA trusted computing module, existing IPFS node can turn to a function-as-a-server node besides storing the code and data.
-   </div>
-<div class="">IPFS store and cache the code, TEA module load the code and execute</div>
-<div class="">TEA protocol provides proof-of-trust (Tea Pot) for verification</div>
-   <a href="">Read the detail</a>
-    <hr/>
-
-    <h3>Take over complex computational workloads from blockchain as a trusted layer-2 oracle</h3>
-<div class="">Smart contract running in blockchain (layer-1) call an external function that runs inside TEA network(layer-2). The function result feed back to smart contract with Proof-of-trust for verification</div>
-   <a href="">Read the detail</a>
-  
-  <hr />
-
-    <h3>Hardware and crypto protected protocol ensures you own your own data</h3>
-<div class="">Encrypt privacy data before upload to IPFS, distribute the decryption key to TEA modules that protected by hardware chips. Only trusted module can access your data after remote attestation</div>
-   <a href="">Read the detail</a>
-    <hr/>
-
-    <h3>Build dApps that censorship-resistent</h3>
-<div class="">Build dApps that encrypted data store in IPFS and code runs in TEA modules. Those kind of dApps cannot be stopped or controlled by any one, even the builder. </div>
-   <a href="">Read the detail</a>
-  </el-row> </div> -->
   <p style="margin-top: 120px;">
     <img src="/img/Under_Construction_Tape.png" width="100%"/>
   </p>
