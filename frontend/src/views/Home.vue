27b6b1365e504f5c4328b1c0878334fa1c920fb5
jacky.li <jacky.li@accenture.com>
2020-12-15 00:51:07 +0800

    update

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -6,7 +6,7 @@
 
   <div style="display:flex; flex-direction:column; align-items:center; justify-content:center; padding: 24px 0 40px; margin: 0 auto; max-width: 800px;">
     <h1 style="font-size: 56px; color: #444;font-weight: bold; text-align:center; font-family:sans-serif; line-height:65px;">Elevating Decentralized Trusted Computing to a T</h1>
-    <p style="font-size: 21px; color: #111; text-align:center; line-height: 28px; margin-top: 15px;">
+    <p style="font-size: 19px; color: #111; text-align:center; line-height: 28px; margin-top: 15px;">
       <a href="http://t-rust.org/#/doc_list/What_is_TEA%3F%2FREADME.md">
         TEA (Trusted Execution and Attestation)
       </a> 
@@ -19,7 +19,7 @@
     <hr>
     <p class="m-11">
       <el-button  size="large" round style="width: 200px; font-weight: bold; font-size: 21px;" type="success" @click="$router.push('/doc_list')" >Documents</el-button>
-      <el-button  size="large" round style="width: 200px; margin-left: 80px; font-weight: bold; font-size: 21px;" type="success" @click="$router.push('/demo')">Demo</el-button>
+      <el-button  size="large" round style="width: 200px; margin-left: 80px; font-weight: bold; font-size: 21px;" type="success" href="/doc_list/Try_the_demo%2FREADME.md" target="_self">Demo</el-button>
     </p>
     
   </div>
